1ef66329fb6d03ba263201f0d5e0c1540c64f3f6
para29 <65754533+para29@users.noreply.github.com>
2021-02-28 15:23:27 +0000

    Wizard

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class NumberWizard : MonoBehaviour
+{
+   [SerializeField] int max;
+   [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+   
+   int guess;
+   // public UnityEngine.UI.Button yourButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+
+        NextGuess();
+        Random.Range(min, max);
+       
+       
+        //max = max + 1;
+    }
+
+    void NextGuess()
+    {
+       // StartGame();
+       
+        guess = Random.Range(min, max);
+        guessText.text = guess.ToString();
+    /*  if(guess < max)
+        {
+          
+            guess++;
+           
+        } if( guess > min)
+        {
+            guess--;
+           
+            
+        }*/
+       // Debug.Log("Is it higher or lower than..." + guess);
+    }
+
+    public void OnPressHigher()
+    {
+       
+        min = guess;
+       // Random.Range(min, max);
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+       // Random.Range(min, max);
+        NextGuess();
+    }
+
+
+    // Update is called once per frame
+
+    /*  if (Input.GetKeyDown(KeyCode.UpArrow))
+      {
+          min = guess;
+          NextGuess();
+      }
+      else if (Input.GetKeyDown(KeyCode.DownArrow))
+      {
+          max = guess;
+          NextGuess();
+      }
+      else if (Input.GetKeyDown(KeyCode.Return))
+      {
+          Debug.Log("I am a genius!");
+          StartGame();
+      }
+  }
+  void NextGuess()
+  {
+      guess = (max + min) / 2;
+      Debug.Log("Is it higher or lower than..." + guess);
+  }*/
+}
+
+
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+   
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene("Start Menu");
+
+       //ou StartManager.LoadScene(0)
+
+
+    }
+
+    public void QuitGame()
+    {
+        /*int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(3);*/
+
+       
+            Application.Quit();
+        
+
+        //ou StartManager.LoadScene(0)
+
+
+    }
+
+
+
+}
